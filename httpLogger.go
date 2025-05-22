3224d881f98e20e0d7d582e99ce9cca29d903aa4
Jeremy Mill <jmill@varomoney.com>
2025-05-22 11:27:14 -0400

    update header logging

--- httpLogger.go
+++ httpLogger.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net"
 	"net/http"
 
@@ -8,15 +9,16 @@ import (
 )
 
 func addLogData(r *http.Request, e *zerolog.Event, msg string) {
-	headersMap := make(map[string][]string)
 	for name, values := range r.Header {
-		headersMap[name] = values // Taking the first value for simplicity
+		for i, v := range values {
+			oname := fmt.Sprintf("%s-%d", name, i)
+			e.Str(oname, v)
+		}
 	}
 
 	e.Str("method", r.Method)
 	e.Str("path", r.URL.Path)
 	e.IPAddr("IP", net.IP(r.RemoteAddr))
-	e.Fields(headersMap)
 
 	e.Msg(msg)
 }
